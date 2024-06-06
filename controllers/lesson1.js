4a0c8bb939e25dd4b9e8a37c19c25cacb900ba48
darvfus <romerondan123@gmail.com>
2024-06-05 19:59:16 -0600

    "change for1"

---
+++ controllers/lesson1.js
@@ -0,0 +1,12 @@
+const emilyRoute=(req, res) => {
+res.send('emily Birch')
+};
+const hannahRoute=(req, res) => {
+    res.send('Hannah Birch')
+    };
+
+    module.exports ={
+        emilyRoute,
+        hannahRoute,
+    };
+    
--- server.js
+++ server.js
@@ -1,16 +1,13 @@
-// server.js
-
 const express = require('express');
 const app = express();
 const port = 3000; // You can choose any port you prefer
+const lesson1Controller = require('./controllers/lesson1');
 
 // Define a simple route
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
-});
+app.get('/', lesson1Controller,emilyRouter);
+app.get('/hannah', lesson1Controller,hannahRoute);
 
 // Start the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+app.listen(process.env.port || port);
+  console.log(`Web serve is listening at port ` + (process.env.port || port));
 
